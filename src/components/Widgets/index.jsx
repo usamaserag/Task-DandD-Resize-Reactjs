e3eb27829ce9270e54bbc1b2674219819f721864
usamaserag <usamaserageldeen@gmail.com>
2022-01-23 19:26:03 +0200

    Created charts components

--- src/components/Widgets/index.jsx
+++ src/components/Widgets/index.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import './styles.scss';
-
+import React from "react";
+import "./styles.scss";
+import ChartLine from "../ChartLine";
+import ChartBar from "../ChartBar";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
 
 const Widgets = () => {
   return (
-      <div className='widgets'>
-          <div className="widgets__wrapper">
-            Widgets
-          </div>
+    <div className="widgets">
+      <Button
+        variant="outlined"
+        startIcon={<AddIcon style={{ fontWeight: "bold" }} />}
+        className="btn"
+      >
+        New Widget
+      </Button>
+      <div className="widgets__wrapper">
+        <ChartLine />
+        <ChartBar />
       </div>
+    </div>
   );
 };
 
