be0ac4425cec0e35567e1fa4aeb5acf3d09d7998
usamaserag <usamaserageldeen@gmail.com>
2022-01-24 23:07:20 +0200

    Handled re-sizing

--- src/components/Charts/index.jsx
+++ src/components/Charts/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {
   AreaChart,
   Area,
-  XAxis,
+  // XAxis,
   // YAxis,
   CartesianGrid,
   ResponsiveContainer,
--- src/components/Drop/index.jsx
+++ src/components/Drop/index.jsx
@@ -3,10 +3,20 @@ import "./styles.scss";
 import { useDrop } from "react-dnd";
 import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
 import { Chart } from "../Charts";
+import { Resizable } from "re-resizable";
+
+const style = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "solid 1px #ddd",
+};
 
 const Drop = ({ data }) => {
   const [dropItems, setDropItems] = useState([]);
-  const [post, setPost] = useState(false)
+
+  const [post, setPost] = useState(false);
+
   const [{ isOver }, drop] = useDrop(() => ({
     accept: "chart",
     drop: (item) => addChartToDrop(item.id),
@@ -18,20 +28,28 @@ const Drop = ({ data }) => {
   const addChartToDrop = (id) => {
     const dropItem = data[id];
     setDropItems([dropItem]);
-    setPost(!post)
+    setPost(!post);
   };
   return (
     <div className="drop">
       <h2 className="title">Drag a widget into an open Dashboard slot</h2>
       <div className="drop__add">
-        <div className={`drop__add__box ${isOver && 'droping'}`} ref={drop}>
-          {post ? (
-            dropItems.map((item, i) => <Chart data={item} key={i} />)
-          ) : (
-            <div className="drop__add__item">
-              <AddCircleOutlinedIcon className="icon-add" />
-            </div>
-          )}
+        <div className={`drop__add__box ${isOver && "droping"}`} ref={drop}>
+          <Resizable
+            style={style}
+            defaultSize={{
+              width: 400,
+              height: 240,
+            }}
+          >
+            {post ? (
+              dropItems.map((item, i) => <Chart data={item} key={i} />)
+            ) : (
+              <div className="drop__add__item">
+                <AddCircleOutlinedIcon className="icon-add" />
+              </div>
+            )}
+          </Resizable>
         </div>
         <div className="drop__add__item">
           <AddCircleOutlinedIcon className="icon-add" />
