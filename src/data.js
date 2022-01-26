29b48297894041c9ef93d0182e9fcecc2e01d20a
usamaserag <usamaserageldeen@gmail.com>
2022-01-26 15:51:07 +0200

    Change line chart data

--- src/data.js
+++ src/data.js
@@ -2,85 +2,104 @@ export const dataCharts = [
   [
     {
       name: "A",
-      uv: 4000,
-      // pv: 2400,
-      // amt: 2400,
+      uv: 2000,
     },
     {
       name: "B",
       uv: 3000,
-      // pv: 1398,
-      // amt: 2210,
     },
     {
       name: "C",
-      uv: 2000,
-      // pv: 9800,
-      // amt: 2290,
+      uv: 5800,
     },
     {
       name: "D",
       uv: 2780,
-      // pv: 3908,
-      // amt: 2000,
     },
     {
       name: "E",
-      uv: 1890,
-      // pv: 4800,
-      // amt: 2181,
+      uv: 3890,
     },
   ],
   [
     {
-      name: "Jan",
+      "name": "Page A",
+      "uv": 1000,
+      "pv": 1900
+    },
+    {
+      "name": "Page B",
+      "uv": 2000,
+      "pv": 3398
+    },
+    {
+      "name": "Page C",
+      "uv": 2500,
+      "pv": 3800
+    },
+    {
+      "name": "Page D",
+      "uv": 5780,
+      "pv": 4908
+    },
+    {
+      "name": "Page E",
+      "uv": 3890,
+      "pv": 4800
+    },
+    {
+      "name": "Page F",
+      "uv": 2390,
+      "pv": 2900
+    },
+    {
+      "name": "Page G",
+      "uv": 1490,
+      "pv": 2300
+    }
+  ],
+  [
+    {
+      name: "Page A",
       uv: 4000,
       pv: 2400,
       amt: 2400,
     },
     {
-      name: "Feb",
+      name: "Page B",
       uv: 3000,
       pv: 1398,
       amt: 2210,
     },
     {
-      name: "Mar",
+      name: "Page C",
       uv: 2000,
       pv: 9800,
       amt: 2290,
     },
     {
-      name: "Apr",
+      name: "Page D",
       uv: 2780,
       pv: 3908,
       amt: 2000,
     },
-  ],
-  [
     {
-      name: "A",
-      uv: 9000,
-      pv: 2400,
-      // amt: 2400,
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
     },
     {
-      name: "B",
-      uv: 6000,
-      pv: 1398,
-      // amt: 2210,
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
     },
     {
-      name: "C",
-      uv: 5000,
-      pv: 1800,
-      // amt: 2290,
-    },
-    {
-      name: "D",
-      uv: 2780,
-      pv: 3908,
-      // amt: 7000,
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
     },
   ],
 ];
